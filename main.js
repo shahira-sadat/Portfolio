4bf2bbc010b57730c0aac1a2f121cb7bf574ca97
shahira-sadat <shahira.sadat1@gmail.com>
2022-06-15 17:22:35 +0430

    Add js mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";}
+const menuItem = document.querySelector('.mobile-item');
+const closeMobileItems = document.querySelectorAll('.closeMenu');
+const displayMobileMenu = () => {
+    menuItem.style.display = 'block';
+};
+const closeMobileMenu = () => {
+    menuItem.style.display = 'none';
+};
+document.querySelector('.openMenu').addEventListener('click', displayMobileMenu);
+closeMobileItems.forEach(((element) => element.addEventListener('click', closeMobileMenu)));
